bad68ff3166cbb260c4d7ae6efe2c4948cfff1d7
MrAndrewlol <40283828+MrAndrewlol@users.noreply.github.com>
2023-09-05 07:56:53 -0600

    Update: Warm colors

--- app/src/main/java/com/example/projecttodolist/ui/theme/Color.kt
+++ app/src/main/java/com/example/projecttodolist/ui/theme/Color.kt
@@ -12,3 +12,7 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 //Paleta de colores del proyecto
+val darkblue = Color(red = 0, green = 18, blue = 66)
+val gray = Color(red = 224, green = 224, blue = 224)
+val blue = Color(red = 88, green = 99,blue = 248 )
+val green = Color(red = 95, green = 116, blue = 112)
