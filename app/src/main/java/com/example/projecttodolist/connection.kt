b1d48e2c6c542862d93c9349cac8cce20ae7d5ce
MrAndrewlol <40283828+MrAndrewlol@users.noreply.github.com>
2023-10-16 10:08:04 -0600

    API REQUEST

--- app/src/main/java/com/example/projecttodolist/connection.kt
+++ app/src/main/java/com/example/projecttodolist/connection.kt
@@ -2,11 +2,14 @@ package com.example.projecttodolist
 
 import android.content.Context
 import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
 import org.json.JSONObject
 
 class dataconnection(){
@@ -21,23 +24,22 @@ class dataconnection(){
 }
 
 
-fun loginrequest(context: Context):String {
-    var dataconnection = dataconnection()
-    val queue = Volley.newRequestQueue(context)
-    val url = "http://127.0.0.1/login/login.php"
-    var response = ""
+@Composable //Agregue el composable
+fun loginrequest() {
+    val urllink = "http://10.0.2.2:8085/datatodo/mostrardatos.php" //Utilizado SOLAMENTE CON EMULADOR
+    val queue: RequestQueue = Volley.newRequestQueue(LocalContext.current) //En caso de emergencia el cambio fue de LocalContext.current
 
-    val stringRequest = StringRequest(Request.Method.GET,
-        url, {
-            // Handling Success
-            Log.d("Success", "simpleRequest:${it}")
-            response = it
 
-        }, {
-            // Handling Error
-            Log.d("Error", "simpleRequest:${it}")
-            response = "Error"
-        })
-    queue.add(stringRequest)
-    return response
+    val request = StringRequest(
+        Request.Method.GET,urllink,
+        { response ->
+
+            val data = JSONArray(response).toString()
+
+            Log.e("Objects",data)
+
+
+        },
+        { Log.e("Objects","sin conex") })
+    queue.add(request)
 }
--- app/src/main/java/com/example/projecttodolist/screens/LogInScreen.kt
+++ app/src/main/java/com/example/projecttodolist/screens/LogInScreen.kt
@@ -1,5 +1,6 @@
 package com.example.projecttodolist.screens
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -26,6 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,11 +35,16 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.projecttodolist.Activities.DrawShape
 import com.example.projecttodolist.Navigation.AppScreens
 import com.example.projecttodolist.ui.theme.blue
 import com.example.projecttodolist.ui.theme.gray
 import com.example.projecttodolist.ui.theme.green
+import org.json.JSONArray
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -142,5 +149,24 @@ fun LogInScreen(navController: NavController) {
 fun LogInPrev() {
     val navController = rememberNavController()
     LogInScreen(navController = navController)
+}
+@Composable //Agregue el composable
+fun loginrequest() {
+    val urllink = "http://10.0.2.2:8085/datatodo/mostrardatos.php" //Utilizado SOLAMENTE CON EMULADOR
+    val queue: RequestQueue = Volley.newRequestQueue(LocalContext.current) //En caso de emergencia el cambio fue de LocalContext.current
+
+
+    val request = StringRequest(
+        Request.Method.GET,urllink,
+        { response ->
+
+            val data = JSONArray(response).toString()
+
+            Log.e("Objects",data)
+
+
+        },
+        { Log.e("Objects","sin conex") })
+    queue.add(request)
 }
 
