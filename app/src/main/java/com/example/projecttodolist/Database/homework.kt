f7665da9d1a6fcfa81c7cc6b0a6d131880e5b407
MrAndrewlol <40283828+MrAndrewlol@users.noreply.github.com>
2023-11-14 08:27:08 -0600

    Funciones para user

--- app/src/main/java/com/example/projecttodolist/Database/homework.kt
+++ app/src/main/java/com/example/projecttodolist/Database/homework.kt
@@ -1,5 +1,73 @@
 package com.example.projecttodolist.Database
 
-fun creatework(){
-    
+import android.util.Log
+import com.example.projecttodolist.GlobalVariables
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+data class homework(
+    var userId: String? = null,
+    var username: String? = null,
+    var password: String? = null,
+    var email: String? = null,
+    var createduser: String? = null,
+    var arraytodo: String? = null, //ARRAY sera ahora para agregar grupos
+    var grupos: String = "-$userId", //This is for groups
+    var settings: String?= null, //
+    var token: String?= null )
+
+fun  createtarea( username: String, password: String, email: String, idusername:String):Boolean { //Funcion registrar datos a REALTIME
+    val dbRef = FirebaseDatabase.getInstance().getReference("tareas")
+    var state: Boolean
+    if (username.isEmpty() or password.isEmpty() or email.isEmpty()) {
+        Log.e("empty data", "Los campos ingresados estan vacio")
+        state = false
+    }else{
+        state = true
+        val currentDate = LocalDate.now()
+        // Define a custom date format
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+
+        // Format the current date using the custom format
+        val formattedDate = currentDate.format(formatter)
+
+        //PARA CREAR FECHA DE CREACION ARRIBA
+
+        GlobalVariables.userdat.userId = idusername
+        GlobalVariables.userdat.password = password
+        GlobalVariables.userdat.email = email
+        GlobalVariables.userdat.createduser =formattedDate.toString()
+        GlobalVariables.userdat.arraytodo = "No Groups"
+
+        //UTILIZACION DE API JWTS
+        val compactJws: String = Jwts.builder()
+            .claim("UserID", GlobalVariables.userdat.userId.toString())
+            .claim("username", GlobalVariables.userdat.username.toString())
+            .claim("password", GlobalVariables.userdat.password.toString())
+            .claim("email", GlobalVariables.userdat.email.toString())
+            .claim("arraytodo", GlobalVariables.userdat.arraytodo.toString())
+            .claim("createduser", GlobalVariables.userdat.createduser.toString())
+            .claim("grupos", GlobalVariables.userdat.grupos.toString())
+            .signWith(SignatureAlgorithm.HS256, "secret".toByteArray())
+            .compact()
+
+        GlobalVariables.userdat.token = compactJws
+
+        dbRef.child(GlobalVariables.userdat.userId.toString()).setValue(GlobalVariables.userdat)
+            .addOnCompleteListener {
+                Log.e("Succesful adding", "Se ha ingresado con exito")
+
+            }.addOnFailureListener {
+                Log.e("Unsuccesful adding", "Se ha ingresado con exito")
+            }
+
+
+    }
+
+    return state
+
 }
--- app/src/main/java/com/example/projecttodolist/Database/usermodel.kt
+++ app/src/main/java/com/example/projecttodolist/Database/usermodel.kt
@@ -1,7 +1,6 @@
 package com.example.projecttodolist.Database
 
 import android.util.Log
-import android.widget.Toast
 import com.example.projecttodolist.GlobalVariables.userdat
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -22,17 +21,18 @@ data class UserModel(
     var email: String? = null,
     var createduser: String? = null,
     var arraytodo: String? = null, //ARRAY sera ahora para agregar grupos
-    var settings: String?= null,
+    var grupos: String = "-$userId", //This is for groups
+    var settings: String?= null, //
     var token: String?= null )
 
 
 //Registrar
 
  lateinit var dbRef: DatabaseReference
-    fun  save( username: String?, password: String?, email: String?, idusername:String):Boolean { //Funcion registrar datos a REALTIME
+    fun  save( username: String, password: String, email: String, idusername:String):Boolean { //Funcion registrar datos a REALTIME
         val dbRef = FirebaseDatabase.getInstance().getReference("usuario")
         var state: Boolean
-        if (username.toString().isEmpty() or password.toString().isEmpty() or email.toString().isEmpty()) {
+        if (username.isEmpty() or password.isEmpty() or email.isEmpty()) {
             Log.e("empty data", "Los campos ingresados estan vacio")
             state = false
         }else{
@@ -47,8 +47,8 @@ data class UserModel(
             //PARA CREAR FECHA DE CREACION ARRIBA
 
             userdat.userId = idusername
-            userdat.password = password.toString()
-            userdat.email = email.toString()
+            userdat.password = password
+            userdat.email = email
             userdat.createduser =formattedDate.toString()
             userdat.arraytodo = "No Groups"
 
@@ -57,9 +57,10 @@ data class UserModel(
                 .claim("UserID", userdat.userId.toString())
                 .claim("username", userdat.username.toString())
                 .claim("password", userdat.password.toString())
-                .claim("email", userdat.email)
-                .claim("grupos", userdat.arraytodo)
-                .claim("createduser", userdat.createduser)
+                .claim("email", userdat.email.toString())
+                .claim("arraytodo", userdat.arraytodo.toString())
+                .claim("createduser", userdat.createduser.toString())
+                .claim("grupos", userdat.grupos.toString())
                 .signWith(SignatureAlgorithm.HS256, "secret".toByteArray())
                 .compact()
 
@@ -82,12 +83,11 @@ data class UserModel(
 
 
 fun deleteRecord(
-    idtarget: String, location: Int, idusuario:String //usuario principal no al que se elimina
+     location: Int, idusuario:String //usuario principal no al que se elimina
 ){
-    if (location == 0){ //FUNCION PARA ELIMINAR DATOS location significa en donde esta por ejemplo si esta en settings y elimina la cuente su localizacion es 0, por lo tanto eleimina la cuenta.
+    if (location == 0){ //FUNCION PARA ELIMINAR DATOS location significa en donde esta por ejemplo si esta en settings y elimina la cuenta en su localizacion es 0, por lo tanto eleimina la cuenta.
         val dbRef = FirebaseDatabase.getInstance().getReference("usuario").child(idusuario)
         val mTask = dbRef.removeValue()
-        eliminarusuarioauth(idusuario) //de fireauthentication
 
         mTask.addOnSuccessListener {
             Log.e("DELETED USER", "Se ha eliminado la cuenta")
@@ -96,13 +96,74 @@ fun deleteRecord(
             Log.e("USER STILL EXIST", "No se ha eliminado la cuenta")
         }
     }
-    if(location == 1){
-            val dbRef = FirebaseDatabase.getInstance().getReference("usuario").child(idusuario)
-            val settings = userdat.settings.toString().replace(idtarget, "")
-            userdat.settings = settings
-            val userinfo  = userdat
-            dbRef.setValue(userinfo)
 
+}
+
+
+fun deletegroupuser(
+    idtarget: String, location: Int, idusuario:String //usuario principal no al que se elimina
+){
+        val dbRef = FirebaseDatabase.getInstance().getReference("usuario").child(idusuario)
+        val grupo = userdat.grupos.replace("-$idtarget", "")
+        userdat.grupos = grupo
+        //userdat  = UserModel(userdat.userId, userdat.username, userdat.password, userdat.email, userdat.createduser,
+            //userdat.arraytodo, userdat.grupos, userdat.settings, userdat.token)
+        dbRef.setValue(userdat)
+}
+
+fun addgroup(
+    idtarget: Int, idusuario:String //usuario principal no al que se elimina
+){
+    val dbRef = FirebaseDatabase.getInstance().getReference("usuario").child(idusuario)
+    userdat.grupos = userdat.grupos + "-" + idtarget
+    dbRef.setValue(userdat)
+}
+
+
+fun setvalue(
+    valuetarget: String, location: String, idusuario:String //value target es el string que se desea cambiar y location
+){
+    val dbRef = FirebaseDatabase.getInstance().getReference("usuario").child(idusuario)
+    //Username
+    if (location == "username"){
+        userdat.username = valuetarget
+        dbRef.setValue(userdat)
     }
+    if (location == "password"){
+        userdat.password = valuetarget
+        dbRef.setValue(userdat)
+    }
+    if (location == "email"){
+        userdat.email = valuetarget
+        dbRef.setValue(userdat)
+    }
+    if (location == "arraytodo"){
+        userdat.arraytodo = valuetarget
+        dbRef.setValue(userdat)
+    }
+
+}
+
+fun decodeJWT(token: String) {
+
+    val secretKey = "secret" // Make sure it matches the key used for signing the token
+
+
+    val claims = Jwts.parser()
+        .setSigningKey(secretKey.toByteArray())
+        .parseClaimsJws(token)
+        .body
+
+    // Access the claims
+
+    // Access the claims
+    userdat.userId = claims.get("UserID", String::class.java)
+    userdat.username = claims.get("username", String::class.java)
+    userdat.password = claims.get("password", String::class.java)
+    userdat.email = claims.get("email", String::class.java)
+    userdat.grupos = claims.get("grupos", String::class.java)
+
+
+
 }
 
