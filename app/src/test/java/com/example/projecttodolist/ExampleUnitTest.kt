666cbae3c0bf879a640ef0bc17dbc5b91fb608d9
MrAndrewlol <40283828+MrAndrewlol@users.noreply.github.com>
2023-11-18 23:28:42 -0600

    Final Commit plus video in a moment

--- app/src/test/java/com/example/projecttodolist/ExampleUnitTest.kt
+++ app/src/test/java/com/example/projecttodolist/ExampleUnitTest.kt
@@ -1,8 +1,13 @@
 package com.example.projecttodolist
 
+import androidx.navigation.NavController
+import com.example.projecttodolist.Navigation.AppScreens
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +19,38 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun typeassignation() {
+        assertEquals("Personal", typeAssignation(true))
+
+    }
+
+    @Test
+    fun duration(){
+        assertEquals("2023-11-18", durationCalc(LocalDate.now().toString() , LocalDate.now().toString()))
+    }
+
+private fun typeAssignation(type : Boolean) : String{
+    if (type) {
+        return "Personal"
+    } else {
+        return "Grupal"
+    }
+
+
+}
+
+   private fun durationCalc(dateI : String, dateF : String): Long {
+        val formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+
+        val fechaI = LocalDate.parse(dateI, formatoFecha)
+        val fechaF = LocalDate.parse(dateF, formatoFecha)
+
+        val duration = ChronoUnit.DAYS.between(fechaI, fechaF) + 1
+
+        return duration
+    }
+
+
 }
